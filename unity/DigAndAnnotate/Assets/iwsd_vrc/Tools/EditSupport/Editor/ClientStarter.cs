f5638f0368270e0340d6b195a55afa554acfd24b
naqtn <your.email@example.com>
2019-07-06 20:26:00 +0900

    Add handling of manually inputed world ID
    * DONE Add TextField for world id and manually open it (need?)
    * DONE add advanced option section ("more options") to hide options that is not used so often
    * DONE show world id as copyable string

--- unity/DigAndAnnotate/Assets/iwsd_vrc/Tools/EditSupport/Editor/ClientStarter.cs
+++ unity/DigAndAnnotate/Assets/iwsd_vrc/Tools/EditSupport/Editor/ClientStarter.cs
@@ -27,7 +27,7 @@
 using UnityEngine;
 using UnityEditor;
 using System;
-
+    
 /**
  * VRChat Client Starter
  *
@@ -37,12 +37,13 @@ using System;
  *
  * After installation, open window via Unity menu Window > VRC_Iwsd > Client Starter
  *
- * When "Start Start" option is enabled, this closes VRChat SDK "Manage World in Browser" dialog.
+ * When "Auto Start" option is enabled, this closes VRChat SDK "Manage World in Browser" dialog.
  * Disenable it if you need to use that dialog.
  *
  * Other features
- * - Start manually.
- * - Open manage page with Web browser
+ * - Start client manually
+ * - Open manage page at vrchat.com
+ * - "Auto Start" feature works even if setting window is not opened
  *
  *
  * Written by naqtn (https://twitter.com/naqtn)
@@ -52,11 +53,10 @@ using System;
  *
  * TODO hide "not logged in" warning when IsLoggedInWithCredentials becomes true (need?)
  * TODO show URL as copyable string (need?)
- * TODO show world id as copyable string (need?)
  * TODO preserve nonce and instance number and reasonably refresh, to meet players by multiple invoke
- * TODO add advanced option section to hide options that is not used so often like above (copyable string, nonce...).
  * TODO Add non-VR selection feature (need directly starting client instead of Application.OpenURL())
- * TODO Add TextField for world id and manually open it (need?)
+ * TODO validate inputed ID string (cut from tail to be good if public URL, or use regex)
+ * BUG no need to be logged for public access instance
  */
 namespace Iwsd
 {
@@ -70,53 +70,109 @@ namespace Iwsd
             EditorWindow.GetWindow<ClientStarterWindow>("VRC Client");
         }
 
-        void OnEnable()
-        {
-        }
-
+        bool moreOptions = false;
+        // "wrld_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+        string manualInputId = "(input world ID: wrld_xxx...)"; // I choiced not to be saved
+        ClientStarter.Result result2 = new ClientStarter.Result(null, true, "");
+            
         void OnGUI ()
         {
             var settings = ClientStarter.settings;
+            var result1 = ClientStarter.lastResult;
 
-            //// Label
+            /// Label
+            EditorGUILayout.BeginHorizontal();
             EditorGUILayout.LabelField("VRChat Client Starter", new GUIStyle(){fontStyle = FontStyle.Bold});
-
-            //// Settings
+            moreOptions = EditorGUILayout.ToggleLeft("more options", moreOptions);
+            EditorGUILayout.EndHorizontal();
+                           
+            /// Settings
             EditorGUI.BeginChangeCheck();
             settings.startAfterPublished
                 = EditorGUILayout.Toggle(new GUIContent("Auto Start", "Start client after publish completed"),
                                          settings.startAfterPublished);
-            settings.worldAccessLevel
-                = (ClientStarter.WorldAccessLevel) EditorGUILayout.EnumPopup("Access",
-                                                                                    settings.worldAccessLevel);
+
+            settings.worldAccessLevel1 = (ClientStarter.WorldAccessLevel)
+                EditorGUILayout.EnumPopup("Access", settings.worldAccessLevel1);
+
             if (EditorGUI.EndChangeCheck()) {
                 settings.Store();
             }
-
-            //// Operation buttons
+            if (moreOptions)
+            {
+                var rect = EditorGUILayout.GetControlRect(true);
+                EditorGUI.PrefixLabel(rect, new GUIContent("World ID (read only)"));
+                rect.x += EditorGUIUtility.labelWidth;
+                rect.width -= EditorGUIUtility.labelWidth;
+                GUI.enabled = false;
+                EditorGUI.TextField(rect, "");
+                GUI.enabled = true;
+                EditorGUI.SelectableLabel(rect, result1.blueprintId);
+                // EditorGUILayout.TextField("World ID (read only)", result.blueprintId);
+            }
+            
+            /// Operation buttons
             EditorGUILayout.BeginHorizontal();
             EditorGUILayout.PrefixLabel("Operations");
-            if (GUILayout.Button("Start Now"))
+            if (GUILayout.Button("Start Client",  GUILayout.ExpandWidth(false)))
             {
-                ClientStarter.TryToOpenLaunchURL(settings.worldAccessLevel);
+                var r = ClientStarter.TryToOpenLaunchURL(null, settings.worldAccessLevel1);
+                ClientStarter.lastResult = r;
             }
-            if (GUILayout.Button("Open Manage Page"))
+            if (GUILayout.Button("Open Manage Page",  GUILayout.ExpandWidth(false)))
             {
-                ClientStarter.TryToOpenManageURL();
+                var r = ClientStarter.TryToOpenManageURL(null);
+                ClientStarter.lastResult = r;
             }
-            EditorGUILayout.Space();
             EditorGUILayout.EndHorizontal();
 
-
             //// Info
-            var result = ClientStarter.lastResult;
-            if (!result.IsSucceeded)
+            EditorGUILayout.Space();
+            if (!result1.IsSucceeded)
             {
-                EditorGUILayout.HelpBox(result.Value, MessageType.Warning);
+                EditorGUILayout.HelpBox(result1.Value, MessageType.Warning);
             }
+
+            /// Manual input ID
+            if (moreOptions)
+            {
+                /// Section label
+                GUILayout.Space(15);
+                EditorGUILayout.LabelField(new GUIContent(" Start another world:",
+                                                          "To start a world that doesn't relate to editing scene"),
+                                           new GUIStyle(){fontStyle = FontStyle.Bold});
+                EditorGUI.indentLevel++;
+
+                /// Settings 2
+                settings.worldAccessLevel2 = (ClientStarter.WorldAccessLevel)
+                    EditorGUILayout.EnumPopup("Access", settings.worldAccessLevel2);
+
+                manualInputId = EditorGUILayout.TextField("World ID", manualInputId);
+
+                /// Operation buttons 2
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("Operations");
+                if (GUILayout.Button("Start Client",  GUILayout.ExpandWidth(false)))
+                {
+                    result2 = ClientStarter.TryToOpenLaunchURL(manualInputId, settings.worldAccessLevel2);
+                }
+                if (GUILayout.Button("Open Manage Page",  GUILayout.ExpandWidth(false)))
+                {
+                    result2 = ClientStarter.TryToOpenManageURL(manualInputId);
+                }
+                EditorGUILayout.EndHorizontal();
+
+                /// Info 2
+                EditorGUILayout.Space();
+                if (!result2.IsSucceeded)
+                {
+                    EditorGUILayout.HelpBox(result2.Value, MessageType.Warning);
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
         }
-
-
 
     }
     ////////////////////////////////////////////////////////////////////////////////
@@ -139,33 +195,41 @@ namespace Iwsd
         public class Settings
         {
             public bool startAfterPublished;
-            public WorldAccessLevel worldAccessLevel;
+            public WorldAccessLevel worldAccessLevel1;
+            public WorldAccessLevel worldAccessLevel2;
 
+    
             private const string startAfterPublished_key = "Iwsd.ClientStarter.startAfterPublished";
-            private const string worldAccessLevel_key = "Iwsd.ClientStarter.worldAccessLevel";
+            private const string worldAccessLevel1_key = "Iwsd.ClientStarter.worldAccessLevel1";
+            private const string worldAccessLevel2_key = "Iwsd.ClientStarter.worldAccessLevel2";
 
             internal static Settings Load()
             {
                 var o = new Settings();
                 o.startAfterPublished = EditorPrefs.GetBool(startAfterPublished_key, true);
-                o.worldAccessLevel = (WorldAccessLevel)EditorPrefs.GetInt(worldAccessLevel_key, (int)WorldAccessLevel.Friends);
+                o.worldAccessLevel1 = (WorldAccessLevel)EditorPrefs.GetInt(worldAccessLevel1_key, (int)WorldAccessLevel.Friends);
+                o.worldAccessLevel2 = (WorldAccessLevel)EditorPrefs.GetInt(worldAccessLevel2_key, (int)WorldAccessLevel.Friends);
                 return o;
             }
             
             internal void Store()
             {
                 EditorPrefs.SetBool(startAfterPublished_key, this.startAfterPublished);
-                EditorPrefs.GetInt(worldAccessLevel_key, (int)this.worldAccessLevel);
+                EditorPrefs.GetInt(worldAccessLevel1_key, (int)this.worldAccessLevel1);
+                EditorPrefs.GetInt(worldAccessLevel2_key, (int)this.worldAccessLevel2);
             }
         }
 
         public class Result {
             public bool IsSucceeded;
             public string Value;
-            public Result(bool isSucceeded, string value)
+            public string blueprintId;
+            
+            public Result(Result result, bool isSucceeded, string value)
             {
                 IsSucceeded = isSucceeded;
                 Value = value;
+                blueprintId = (result == null)? "---": result.blueprintId;
             }
         }
 
@@ -177,7 +241,7 @@ namespace Iwsd
         
         static ClientStarter()
         {
-            lastResult = new Result(true, "");
+            lastResult = new Result(null, true, "");
             settings = Settings.Load();
 
             EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
@@ -226,8 +290,8 @@ namespace Iwsd
             var completeDialog = Resources.FindObjectsOfTypeAll(typeof(VRCSDK2.ContentUploadedDialog)) as EditorWindow[];
             if (completeDialog.Length != 0)
             {
-                var r = TryToOpenLaunchURL(settings.worldAccessLevel);
-                if (r.IsSucceeded)
+                lastResult = TryToOpenLaunchURL(null, settings.worldAccessLevel1);
+                if (lastResult.IsSucceeded)
                 {
                     completeDialog[0].Close();
                 }
@@ -255,20 +319,26 @@ namespace Iwsd
             return url;
         }
 
-        public static Result TryToOpenManageURL()
+        public static Result TryToOpenManageURL(string id_opt)
         {
-            lastResult = TryToOpen(ComposeManageURL());
-            return lastResult;
+            return TryToOpen(ComposeManageURL(id_opt));
         }
 
-        public static Result TryToOpenLaunchURL(WorldAccessLevel accessLevel)
+        public static Result TryToOpenLaunchURL(string id_opt, WorldAccessLevel accessLevel)
         {
-            lastResult = TryToOpen(ComposeLaunchURL(accessLevel));
-            return lastResult;
+            return TryToOpen(ComposeLaunchURL(id_opt, accessLevel));
         }
 
-        public static Result ExtractSceneBlueprintId()
+        public static Result ExtractSceneBlueprintId(string id_opt)
         {
+            if (id_opt != null)
+            {
+                var r = new Result(null, true, "");
+                // TODO validate id_opt 
+                r.blueprintId = id_opt;
+                return r;
+            }
+            
             var vrcPipelineManager = Resources.FindObjectsOfTypeAll(typeof(VRC.Core.VRCPipelineManager)) as VRC.Core.VRCPipelineManager[];
             foreach (var pm in vrcPipelineManager)
             {
@@ -280,39 +350,41 @@ namespace Iwsd
                     var blueprintId = pm.blueprintId;
                     if (blueprintId == null || blueprintId == "")
                     {
-                        return new Result(false, "Not publishd yet? (blueprintId is empty)");
+                        return new Result(null, false, "Not publishd yet? (blueprintId is empty)");
                     }
-                        
-                    return new Result(true, blueprintId);
+
+                    var r = new Result(null, true, "");
+                    r.blueprintId = blueprintId;
+                    return r;
                 }
             }
-            return new Result(false, "VRC_SceneDescriptor is missing? (vrcPipelineManager.Length=" + vrcPipelineManager.Length + ")");
+            return new Result(null, false, "VRC_SceneDescriptor is missing? (vrcPipelineManager.Length=" + vrcPipelineManager.Length + ")");
         }
         
 
-        public static Result ComposeLaunchURL(WorldAccessLevel accessLevel)
+        public static Result ComposeLaunchURL(string id_opt, WorldAccessLevel accessLevel)
         {
-            var bid = ExtractSceneBlueprintId();
+            var bid = ExtractSceneBlueprintId(id_opt);
             if (!bid.IsSucceeded)
             {
                 return bid;
             }
-            var blueprintId = bid.Value;
+            var blueprintId = bid.blueprintId;
 
             if (!VRC.Core.APIUser.IsLoggedInWithCredentials)
             {
-                return new Result(false, "Not logged in. (Open 'VRChat SDK/Settings to check and try again' )");
+                return new Result(bid, false, "Not logged in. (Open 'VRChat SDK/Settings to check and try again' )");
             }
 
             var user = VRC.Core.APIUser.CurrentUser;
             if (user == null)
             {
-                return new Result(false, "user == null");
+                return new Result(bid, false, "user == null");
             }
             var userid = user.id;
             if (userid == null)
             {
-                return new Result(false, "user.id == null");
+                return new Result(bid, false, "user.id == null");
             }
 
             var nonce = Guid.NewGuid();
@@ -334,16 +406,16 @@ namespace Iwsd
                 }
             }
 
-            return new Result(true, url);
+            return new Result(bid, true, url);
         }
 
-        public static Result ComposeManageURL()
+        public static Result ComposeManageURL(string id_opt)
         {
-            var bid = ExtractSceneBlueprintId();
+            var bid = ExtractSceneBlueprintId(id_opt);
             if (bid.IsSucceeded)
             {
                 // https://vrchat.com/home/world/{blueprintId}
-                return new Result(true, "https://vrchat.com/home/world/" + bid.Value);
+                return new Result(bid, true, "https://vrchat.com/home/world/" + bid.blueprintId);
             }
             return bid;
         }
