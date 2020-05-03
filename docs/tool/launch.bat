68e39469dcd8af98690faebe13e131bc5633d575
naqtn <your.email@example.com>
2020-05-03 14:01:03 +0900

    Fix failing to invoke if VRChat installed path contains space character.
    Thx kohack_v

--- docs/tool/launch.bat
+++ docs/tool/launch.bat
@@ -184,7 +184,7 @@ if "%log_sw%"=="on" (
   set opt_log=--enable-debug-gui --enable-sdk-log-levels --enable-udon-debug-logging
 )
 
-if not "%1"=="" (
+if not "%~1"=="" (
   cd /d %1
 )
 %opt_start% VRChat.exe %opt_no_vr% %opt_log% --profile=%profile_no% %2
